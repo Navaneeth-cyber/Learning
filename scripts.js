e1d42ac42734645d913479edfde4038893d79eef
ℕᴀᴠᴀɴᴇᴇᴛʜ࿐ <P.navaneethan2005@gmail.com>
2024-11-14 15:08:26 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -47,3 +47,14 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+// Smooth Scrolling for Menu Links
+document.querySelectorAll('.menu a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+    
